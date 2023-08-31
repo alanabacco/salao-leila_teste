c13a3286899fdc3f78e3b7dea21e96257f939310
Alana Bacco <alana_bacco@hotmail.com>
2023-08-31 13:18:55 -0300

    server: create swagger WIP

--- server/src/app.js
+++ server/src/app.js
@@ -5,6 +5,10 @@ const cors = require("cors");
 
 const app = express();
 
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 const port = process.env.PORT || 8080;
 
 app.use(
