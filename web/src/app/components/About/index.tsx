51df350a79a9d6b008939e122c5e7928ebe7e2dc
Alana Bacco <alana_bacco@hotmail.com>
2023-08-31 00:42:43 -0300

    web: wip, styling components

--- web/src/app/components/About/index.tsx
+++ web/src/app/components/About/index.tsx
@@ -4,7 +4,7 @@ export default function About(): JSX.Element {
   return (
     <>
       <img src="https://placehold.co/500x300" alt="foto" className={styles.image} />
-      <p className={styles.abaoutDescription}>
+      <p className={styles.aboutDescription}>
         Cabelos, unhas, hidratação e unha. Venha fazer suas unhas, seus cabelos, e até
         mesmo hidratar suas madeixas de cabelo conosco. Tudo esterelizado.
       </p>
--- web/src/app/components/Footer/index.tsx
+++ web/src/app/components/Footer/index.tsx
@@ -5,6 +5,8 @@ export default function Footer(): JSX.Element {
   return (
     <footer className={styles.footer}>
       <Link href="/">2023 - Cabeleleila Leila: Salão de Beleza</Link>
+      <address>Rua Fulano de Tal, 1000, Centro - Borborema/SP</address>
+      <a href="tel:16997544271">{"(16) 99754-4271"}</a>
     </footer>
   );
 }
