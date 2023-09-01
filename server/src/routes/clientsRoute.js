748685a1520631fe372758a2cf4abac0857894a9
Alana Bacco <alana_bacco@hotmail.com>
2023-08-31 22:03:10 -0300

    server: edit swagger

--- server/src/routes/clientsRoute.js
+++ server/src/routes/clientsRoute.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
 const ClientsController = require("../controllers/ClientsController");
+const authenticator = require("../middlewares/authenticator");
 
 const router = Router();
 
-router.get("/clients", ClientsController.getClients);
+router.get("/clients", authenticator, ClientsController.getClients);
 router.post("/clients", ClientsController.createClient);
 
 module.exports = router;
